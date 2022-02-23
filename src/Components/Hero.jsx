b142d4079b2177d4f2c3a79cc17753c97ab3177f
mc yassin <yassin2000ziko@gmail.com>
2022-02-23 15:57:36 +0100

    changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import {BsPlayFill} from 'react-icons/bs'
+import React, { useContext } from 'react';
+import { BsPlayFill } from 'react-icons/bs';
+import { MoviesContext } from '../context/MoviesContext';
 
 const Hero = () => {
+  const { setType, fetchPopular, popular, setPopular } =
+    useContext(MoviesContext);
   return (
-    <div className='w-full flex flex-col md:justify-start justify-center items-start p-10 mt-10'>
-      <h1 className='text-white text-6xl font-bold'>Godzeila</h1>
-      <p className='text-white text-md font-bold sm:w-9/12 mt-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure officia molestiae totam suscipit, veniam dolor animi est temporibus hic facere aperiam ad asperiores aliquid. Fugit velit placeat aliquam illum?</p>
+    <div className="w-full flex flex-col md:justify-start justify-center items-start p-10 mt-10">
+      <h1 className="text-white text-6xl font-bold">Godzeila</h1>
+      <p className="text-white text-md font-bold sm:w-9/12 mt-5">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iure
+        officia molestiae totam suscipit, veniam dolor animi est temporibus hic
+        facere aperiam ad asperiores aliquid. Fugit velit placeat aliquam illum?
+      </p>
 
-      <button className='text-white bg-transparent py-3 px-5  mt-5 border-[2px] border-white rounded-full'><BsPlayFill /> Watch Now</button>
+      <button className="text-white bg-transparent py-2 px-5  mt-5 border-[2px] border-white rounded-full flex justify-center items-center">
+        <BsPlayFill fontSize={20} className="mr-3" /> Watch Now
+      </button>
+      {popular.map((item) => {
+        <h1>item</h1>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -4,26 +4,48 @@ import React, { createContext, useState, useEffect } from 'react';
 export const MoviesContext = createContext();
 
 export const MoviesProvider = ({ children }) => {
+  const [popular, setPopular] = useState([]);
+  const [movie, setMovie] = useState({});
   const [type, setType] = useState('movie');
-  const [popular, setPopular] = useState(null);
   const fetchPopular = async (type) => {
     try {
       const response = await fetch(
-        `https://api.themoviedb.org/3/${type}/popular?api_key=71a7c06b948e1262aec789e5bea4f67b&language=en-US&page=1`
+        `https://api.themoviedb.org/3/${type}/popular?api_key=71a7c06b948e1262aec789e5bea4f67b`
       );
       const data = await response.json();
-      setPopular(data.results)
+      setPopular(data.results);
       console.log(popular);
+      console.log(data.results);
     } catch (error) {
       console.error(error);
     }
   };
+
   useEffect(() => {
-      fetchPopular(type)
-  },[type])
+    fetchPopular(type);
+  }, [type]);
+
+  const fetchMovieWithhId = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/634649?api_key=71a7c06b948e1262aec789e5bea4f67b&language=en-US`
+      );
+      const data = await response.json();
+      setMovie(data);
+      console.log(movie);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovieWithhId();
+  }, []);
 
   return (
-    <MoviesContext.Provider value={{ setType, fetchPopular, popular,setPopular }}>
+    <MoviesContext.Provider
+      value={{ setType, fetchPopular, popular, setPopular, movie }}
+    >
       {children}
     </MoviesContext.Provider>
   );
